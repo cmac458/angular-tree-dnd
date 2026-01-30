eb6f841ffb62d96294d873847a0592e55ffa4094
Chris McGuire <cmac458@gmail.com>
2026-01-30 14:47:19 -0500

    Reduce watch overhead in directives

--- src/directive/angular-tree-dnd-compile.js
+++ src/directive/angular-tree-dnd-compile.js
@@ -5,10 +5,12 @@ angular.module('ntt.TreeDnD')
             return {
                 restrict: 'A',
                 link:     function (scope, element, attrs) {
+                    var lastValue;
                     // Store the deregistration function to prevent memory leaks
                     var unwatchCompile = scope.$watch(
                         attrs.compile, function (new_val) {
-                            if (new_val) {
+                            if (new_val && new_val !== lastValue) {
+                                lastValue = new_val;
                                 if (angular.isFunction(element.empty)) {
                                     element.empty();
                                 } else {
@@ -37,10 +39,12 @@ angular.module('ntt.TreeDnD')
             return {
                 restrict: 'A',
                 link:     function (scope, element, attrs) {
+                    var lastValue;
                     // Store the deregistration function to prevent memory leaks
                     var unwatchCompileReplace = scope.$watch(
                         attrs.compileReplace, function (new_val) {
-                            if (new_val) {
+                            if (new_val && new_val !== lastValue) {
+                                lastValue = new_val;
                                 element.replaceWith($compile(new_val)(scope));
                             }
                         }
